caff13235e1cec02ef6776165b27e7a8fd469947
wei <wei.cencol@gmail.com>
2014-03-30 20:18:57 -0400

    test github with pt [delivers #66269408]

--- src/com/example/hunttwo/SingleListItem.java
+++ src/com/example/hunttwo/SingleListItem.java
@@ -15,9 +15,7 @@ public class SingleListItem extends Activity{
         TextView txtDate = (TextView) findViewById(R.id.ex_date);
          
         Intent i = getIntent();
-        // getting attached intent data
         String product = i.getStringExtra("product");
-        // displaying selected product name
         txtProduct.setText(product);
         txtDate.setText("Expired date: 2014/05/22");
     }
